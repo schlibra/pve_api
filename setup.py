3e098cc9c13862ca6aa240faa0c22009096ede81
schlibra <schlibra@163.com>
2025-07-01 23:02:05 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pve_api",
-    version="1.0",
+    version="1.1",
     author="schlibra",
     author_email="schlibra@163.com",
     description="Python library for Proxmox VE API",
